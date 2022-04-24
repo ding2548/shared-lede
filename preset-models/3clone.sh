e6ec420a7909fcc61b55a0e388a5160a7f44b61f
ding2548 <100104802+ding2548@users.noreply.github.com>
2022-04-24 08:54:16 +0800

    Update 3clone.sh

--- preset-models/3clone.sh
+++ preset-models/3clone.sh
@@ -6,12 +6,6 @@ mv ./openwrt/* ./
 # 下载自定义插件
 mkdir -p package/myapps
 cd package/myapps
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/kenzok8/small-package package/small-package
+git clone https://github.com/kenzok8/small.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git
